1c0ac34dccec68ecf687ce636decca38fd1f4926
Ian Rocha <ianbritosr@gmail.com>
2019-02-22 04:12:26 -0300

    Add filtering to backend beer view

--- beerhouse-backend/beerhouse/settings/local.py
+++ beerhouse-backend/beerhouse/settings/local.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'rest_framework_swagger',
     'corsheaders',
     'storages',
+    'django_filters',
 ]
 
 MIDDLEWARE = [
--- beerhouse-backend/beers/views.py
+++ beerhouse-backend/beers/views.py
@@ -1,3 +1,4 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.viewsets import ModelViewSet
 
 from .serializers import BeerSerializer
@@ -7,4 +8,6 @@ from .models import Beer
 class BeerViewSet(ModelViewSet):
     serializer_class = BeerSerializer
     queryset = Beer.objects.all()
+    filter_backends = (DjangoFilterBackend,)
+    filter_fields = ('name', 'color', 'alcohol_content', 'temperature', 'ingredients')
 
